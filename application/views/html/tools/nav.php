4033041d7116d3ffa147f1e20e404b3f655b1f09
lishuangshuang <li.shuangshuang@immomo.com>
2016-10-02 23:32:46 +0800

    修改文件结尾为.html

--- application/views/html/tools/nav.php
+++ application/views/html/tools/nav.php
@@ -23,9 +23,9 @@
         </div>
         <div class="nav_list">
             <ul class="ul_div">
-                <li class='li_list <?php if(empty($_GET["p"])) {echo "now";}?>'><a href="<?php echo base_url();?>json/index">json格式转换</a></li>
-                <li class='li_list <?php if (isset($_GET["p"]) && $_GET["p"] == 2) {echo "now";}?>'><a href="<?php echo base_url();?>timestamp/index?p=2">时间格式转换</a></li>
-                <li class='li_list <?php if (isset($_GET["p"]) && $_GET["p"] == 3) {echo "now";}?>'><a href="<?php echo base_url();?>regex/index?p=3">正则表达式</a></li>
+                <li class='li_list <?php if(empty($_GET["p"])) {echo "now";}?>'><a href="<?php echo base_url();?>json/index.html">json格式转换</a></li>
+                <li class='li_list <?php if (isset($_GET["p"]) && $_GET["p"] == 2) {echo "now";}?>'><a href="<?php echo base_url();?>timestamp/index.html?p=2">时间格式转换</a></li>
+                <li class='li_list <?php if (isset($_GET["p"]) && $_GET["p"] == 3) {echo "now";}?>'><a href="<?php echo base_url();?>regex/index.html?p=3">正则表达式</a></li>
                 <li class='li_list  has_son <?php if (isset($_GET["p"]) && ($_GET["p"] == 4 || $_GET["p"] == 5 || $_GET["p"] == 6 )) {echo "now";}?>'><a href="#">
                     <?php if(empty($_GET["p"])){
                         echo "编解码";
@@ -44,12 +44,12 @@
 
                 </a>
                     <ul class="son">
-                      <li ><a role="menuitem" tabindex="-1" href="<?php echo base_url();?>Url/index?p=4">URL编码</a></li>
-                      <li><a role="menuitem" tabindex="-1" href="<?php echo base_url();?>base64/index?p=5">Base64编码</a></li>
-                      <li><a role="menuitem" tabindex="-1" href="<?php echo base_url();?>Md5/index?p=6">md5加密</a></li>
+                      <li ><a role="menuitem" tabindex="-1" href="<?php echo base_url();?>Url/index.html?p=4">URL编码</a></li>
+                      <li><a role="menuitem" tabindex="-1" href="<?php echo base_url();?>base64/index.html?p=5">Base64编码</a></li>
+                      <li><a role="menuitem" tabindex="-1" href="<?php echo base_url();?>Md5/index.html?p=6">md5加密</a></li>
                     </ul>
                 </li>
-                <li class='li_list <?php if (isset($_GET["p"]) && $_GET["p"] == 7) {echo "now";}?>'><a href="<?php echo base_url();?>linuxCommand/index?p=7">Linux命令大全</a></li>
+                <li class='li_list <?php if (isset($_GET["p"]) && $_GET["p"] == 7) {echo "now";}?>'><a href="<?php echo base_url();?>linuxCommand/index.html?p=7">Linux命令大全</a></li>
 
             </ul>
         </div>
