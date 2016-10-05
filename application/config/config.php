d9fc7a74d134c0a85660d8c5597cf394f0b5956d
begginghard <begginghard@gmail.com>
2016-10-05 10:21:57 +0800

    add linux cmd

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://123itools.com/';
+$config['base_url'] = 'http://www.123itools.com/';
 #$config['base_url'] = 'http://127.0.0.1/';
 
 /*
