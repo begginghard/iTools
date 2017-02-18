d760ecd9d1e457c4b65ca4f5f4e83e414270ff4c
begginghard <begginghard@gmail.com>
2017-02-18 23:27:04 +0800

    modify weixin

--- application/controllers/Weixin.php
+++ application/controllers/Weixin.php
@@ -6,19 +6,26 @@
  * Time: 下午9:01
  */
 
+include_once "SHA1.php";
+include_once "ErrorCode.php";
+
 defined('BASEPATH') OR exit('No direct script access allowed');
+date_default_timezone_set('Asia/Shanghai');
 
 class Weixin extends CI_Controller {
     public function index() {
-        $timestamp = $_POST['timestamp'];
-        $nonce = $_POST['nonce'];
-        $encrypt = $_POST['encrypt'];
-        $msgSignature = $_POST['signature'];
-        $echostr = $_POST['echostr'];
+	log_message("error", "------test------");
+	var_dump($_REQUEST);
+	var_dump($_GET);
+        $timestamp = $_REQUEST['timestamp'];
+        $nonce = $_REQUEST['nonce'];
+        $msgSignature = $REQUEST['signature'];
+        $echostr = $_REQUEST['echostr'];
         $token = "zgh1988";
+	log_message("info", $timestamp . $nonce . $msgSignature);
         //验证安全签名
         $sha1 = new SHA1;
-        $array = $sha1->getSHA1($token, $timestamp, $nonce, $encrypt);
+        $array = $sha1->getSHA1($token, $timestamp, $nonce);
         $ret = $array[0];
 
         if ($ret != 0) {
